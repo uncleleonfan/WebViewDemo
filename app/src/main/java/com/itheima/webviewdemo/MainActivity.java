bac0840069a4c68918a08310946b37762114112f
Leon <526247082@qq.com>
2016-10-02 17:45:22 +0800

    Add loading progress.

--- app/src/main/java/com/itheima/webviewdemo/MainActivity.java
+++ app/src/main/java/com/itheima/webviewdemo/MainActivity.java
@@ -3,23 +3,29 @@ package com.itheima.webviewdemo;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private WebView mWebView;
 
+    private ProgressBar mProgress;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mWebView = (WebView) findViewById(R.id.web_view);
+        mProgress = (ProgressBar) findViewById(R.id.progress);
 
         mWebView.loadUrl("https://www.baidu.com");
-
         mWebView.setWebViewClient(mWebViewClient);
+        mWebView.setWebChromeClient(mWebChromeClient);
     }
     
     private WebViewClient mWebViewClient = new WebViewClient() {
@@ -32,6 +38,20 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void onPageFinished(WebView view, String url) {
             Toast.makeText(MainActivity.this, "onPageFinished", Toast.LENGTH_SHORT).show();
+        }
+    };
+
+    private WebChromeClient mWebChromeClient = new WebChromeClient() {
+
+        @Override
+        public void onReceivedTitle(WebView view, String title) {
+            Toast.makeText(MainActivity.this, "onReceivedTitle " + title, Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onProgressChanged(WebView view, int newProgress) {
+            mProgress.setVisibility(newProgress == 100 ? View.GONE : View.VISIBLE);
+            mProgress.setProgress(newProgress);
         }
     };
 }
